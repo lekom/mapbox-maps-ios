590c264c5fd8c3550ce7aecad6c5ac93408bdd36
Daniel Eke <daniel.eke@mapbox.com>
2021-11-12 16:17:51 +0200

    Add further test cases to ViewAnnotationTests

--- Tests/MapboxMapsTests/Foundation/ViewAnnotationTests.swift
+++ Tests/MapboxMapsTests/Foundation/ViewAnnotationTests.swift
@@ -32,49 +32,63 @@ final class ViewAnnotationTests: XCTestCase {
     }
     
     func testAddAnnotationViewMissingGeometry() {
-        let testView = UIView()
-        do {
-            _ = try manager.addAnnotationView(withContent: testView, options: ViewAnnotationOptions())
-        } catch ViewAnnotationManagerError.insertionFailure(reason: let reason) {
-            XCTAssertFalse(reason.isEmpty)
-        } catch {
-            XCTFail()
-        }
+        XCTAssertThrowsError(try manager.addAnnotationView(withContent: UIView(), options: ViewAnnotationOptions()))
         XCTAssertEqual(mockMapboxMap.addViewAnnotationStub.invocations.count, 0)
         XCTAssertEqual(container.subviews.count, 0)
     }
     
     func testRemove() {
+        let annotationView = addTestAnnotationView()
+        XCTAssertEqual(mockMapboxMap.removeViewAnnotationStub.invocations.count, 0)
+        XCTAssertEqual(container.subviews.count, 1)
+        XCTAssertNoThrow(try manager.remove(annotationView))
+        XCTAssertEqual(mockMapboxMap.removeViewAnnotationStub.invocations.count, 1)
+        XCTAssertEqual(container.subviews.count, 0)
         
+        // Trying to remove the same view the second time should throw
+        XCTAssertThrowsError(try manager.remove(annotationView))
     }
     
     func testUpdate() {
+        let annotationView = addTestAnnotationView()
+        XCTAssertEqual(mockMapboxMap.updateViewAnnotationStub.invocations.count, 0)
+        let options = ViewAnnotationOptions(width: 10.0, allowOverlap: false, anchor: .bottomRight)
+        XCTAssertNoThrow(try manager.update(annotationView, options: options))
+        XCTAssertEqual(mockMapboxMap.updateViewAnnotationStub.invocations.count, 1)
+        XCTAssertEqual(mockMapboxMap.updateViewAnnotationStub.invocations.first?.parameters.options, options)
         
+        // Trying to update the view after removal should throw
+        XCTAssertNoThrow(try manager.remove(annotationView))
+        XCTAssertThrowsError(try manager.update(annotationView, options: options))
     }
     
     func testViewAnnotationByFeatureId() {
+        let testIdOne = "testIdOne"
+        let testIdTwo = "testIdTwo"
+        let geometry = Geometry.point(Point(CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
+        let options = ViewAnnotationOptions(geometry: geometry, associatedFeatureId: testIdOne)
+        let annotationView = try? manager.addAnnotationView(withContent: UIView(), options: options)
+        mockMapboxMap.optionsForViewAnnotationWithIdStub.defaultReturnValue = options
         
+        XCTAssertEqual(annotationView, manager.viewAnnotation(byFeatureId: testIdOne))
+        XCTAssertNil(manager.viewAnnotation(byFeatureId: testIdTwo))
+        XCTAssertNil(manager.viewAnnotation(byFeatureId: ""))
     }
     
     func testOptionsByFeatureId() {
-        
+        // TODO: implement test case
     }
     
     func testOptionsByAnnotationView() {
-        
+        // TODO: implement test case
     }
     
     func testPlaceAnnotations() {
-        
+        // TODO: implement test case
     }
     
     func testValidateAnnotation() {
-        let testView = UIView()
-        let geometry = Geometry.point(Point(CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
-        guard let annotationView = try? manager.addAnnotationView(withContent: testView, options: ViewAnnotationOptions(geometry: geometry)) else {
-            XCTFail()
-            return
-        }
+        let annotationView = addTestAnnotationView()
 
         // First check: annotation is valid, leave it in place
         manager.validateAnnotation(byAnnotationId: annotationView.id)
@@ -87,12 +101,7 @@ final class ViewAnnotationTests: XCTestCase {
     }
     
     func testAnnotationViewVisibilityUpdate() {
-        let testView = UIView()
-        let geometry = Geometry.point(Point(CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
-        guard let annotationView = try? manager.addAnnotationView(withContent: testView, options: ViewAnnotationOptions(geometry: geometry)) else {
-            XCTFail()
-            return
-        }
+        let annotationView = addTestAnnotationView()
         
         let stub = mockMapboxMap.updateViewAnnotationStub
         XCTAssertEqual(stub.invocations.count, 0)
@@ -102,6 +111,12 @@ final class ViewAnnotationTests: XCTestCase {
         annotationView.isHidden = false
         XCTAssertEqual(stub.invocations.count, 2)
         XCTAssertTrue(stub.invocations.last!.parameters.options.visible!)
+    }
+    
+    func addTestAnnotationView() -> AnnotationView {
+        let geometry = Geometry.point(Point(CLLocationCoordinate2D(latitude: 0.0, longitude: 0.0)))
+        let annotationView = try! manager.addAnnotationView(withContent: UIView(), options: ViewAnnotationOptions(geometry: geometry))
+        return annotationView
     }
     
 }
